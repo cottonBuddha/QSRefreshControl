2fb8af8d68800d1fc1ab1920d2b0ecbdeaab4f93
cottonBuddha <jqs@cottonBuddha.local>
2017-10-13 22:34:46 +0800

    部分代码整理

--- demo/ios/QSRefreshControl/RCTScrollView+QSRefresh.m
+++ demo/ios/QSRefreshControl/RCTScrollView+QSRefresh.m
@@ -19,22 +19,21 @@
  */
 - (void)setRnRefreshView:(QSRefreshControl *)refreshView
 {
-    objc_setAssociatedObject(self, @selector(rnRefreshView), refreshView, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+  objc_setAssociatedObject(self, @selector(rnRefreshView), refreshView, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
 }
 
 - (UIView *)rnRefreshView
 {
-    return objc_getAssociatedObject(self, @selector(rnRefreshView));
+  return objc_getAssociatedObject(self, @selector(rnRefreshView));
 }
 
 - (void)setRefreshViewControl:(QSRefreshControl *)refreshView
 {
-    if (self.rnRefreshView) {
-        [self.rnRefreshView removeFromSuperview];
-    }
-    self.rnRefreshView = refreshView;
-    [self addSubview:refreshView];
-
+  if (self.rnRefreshView) {
+    [self.rnRefreshView removeFromSuperview];
+  }
+  self.rnRefreshView = refreshView;
+  [self addSubview:refreshView];
 }
 
 @end
@@ -43,40 +42,35 @@
 @implementation RCTScrollView (QSRefresh)
 
 + (void)load {
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        RCTSwapInstanceMethods([self class], @selector(insertReactSubview:atIndex:), @selector(replace_insertReactSubview:atIndex:));
-        
-        RCTSwapInstanceMethods([self class], @selector(removeReactSubview:), @selector(replace_removeReactSubview:));
-
-    });
+  static dispatch_once_t onceToken;
+  dispatch_once(&onceToken, ^{
+    RCTSwapInstanceMethods([self class], @selector(insertReactSubview:atIndex:), @selector(replace_insertReactSubview:atIndex:));
+    
+    RCTSwapInstanceMethods([self class], @selector(removeReactSubview:), @selector(replace_removeReactSubview:));
+  });
 }
 
 - (void)replace_insertReactSubview:(UIView *)view atIndex:(NSInteger)atIndex {
-
-    if ([view isKindOfClass:[QSRefreshControl class]]) {
-        [super insertReactSubview:view atIndex:atIndex];
-
-        if ([self.scrollView respondsToSelector:@selector(setRefreshViewControl:)]) {
-            [self.scrollView performSelector:@selector(setRefreshViewControl:) withObject:view];
-        }
-    } else {
-        [self replace_insertReactSubview:view atIndex:atIndex];
-    }
+  if ([view isKindOfClass:[QSRefreshControl class]]) {
+    [super insertReactSubview:view atIndex:atIndex];
     
+    if ([self.scrollView respondsToSelector:@selector(setRefreshViewControl:)]) {
+      [self.scrollView performSelector:@selector(setRefreshViewControl:) withObject:view];
+    }
+  } else {
+    [self replace_insertReactSubview:view atIndex:atIndex];
+  }
 }
 
 - (void)replace_removeReactSubview:(UIView *)subview {
-
-    if ([subview isKindOfClass:[QSRefreshControl class]]) {
-        [super removeReactSubview:subview];
-        if ([self.scrollView respondsToSelector:@selector(setRefreshViewControl:)]) {
-            [self.scrollView performSelector:@selector(setRefreshViewControl:) withObject:nil];
-        }
-    } else {
-        [self replace_removeReactSubview:subview];
+  if ([subview isKindOfClass:[QSRefreshControl class]]) {
+    [super removeReactSubview:subview];
+    if ([self.scrollView respondsToSelector:@selector(setRefreshViewControl:)]) {
+      [self.scrollView performSelector:@selector(setRefreshViewControl:) withObject:nil];
     }
-
+  } else {
+    [self replace_removeReactSubview:subview];
+  }
 }
 
 @end
